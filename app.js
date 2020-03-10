191dad3cb884fe7660f81e1372e8f91b31a3eabd
kwanik-kor <rhksdlr134@naver.com>
2020-03-10 15:22:48 +0900

    set and study Router on Express

--- app.js
+++ app.js
@@ -3,13 +3,9 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
 
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () => 
-console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("Hello from my ass");
 
@@ -21,9 +17,10 @@ app.use(bodyParser.urlencoded({ extended : true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", middleware, handleHome);
+app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
+app.use("/user", userRouter);
 
-app.listen(PORT, handleListening);
+export default app;
