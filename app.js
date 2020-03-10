cc2cca02d359dc1ae16c3e9caefe93b5addb92a1
kwanik-kor <rhksdlr134@naver.com>
2020-03-10 15:50:20 +0900

    get url address from routes const file

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import routes from "./routes";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -14,8 +15,8 @@ app.use(bodyParser.urlencoded({ extended : true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
+import routes from "../routes";
 
 const userRouter = express.Router();
 
-userRouter.get("/", (req, res) => res.send('user index'));
-userRouter.get("/edit", (req, res) => res.send('user edit'));
-userRouter.get("/password", (req, res) => res.send('user password'));
+userRouter.get(routes.users, (req, res) => res.send('user index'));
+userRouter.get(routes.editProfile, (req, res) => res.send('user edit'));
+userRouter.get(routes.changePassword, (req, res) => res.send('user password'));
 
 export default userRouter;
