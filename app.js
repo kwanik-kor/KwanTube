9ddd32e7b46d02a1aedbcbbe31e8f97fd5bd30e7
kwanik-kor <rhksdlr134@naver.com>
2020-03-10 16:30:41 +0900

    layouts with 'pug'

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 const app = express();
 
+app.set('view engine', "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true}));
