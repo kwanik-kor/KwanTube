85311468806623bc4261d9061711f0ee8fbb40bf
kwanik-kor <rhksdlr134@naver.com>
2020-03-10 17:21:14 +0900

    create layouts and apply it to each page

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,10 +1,9 @@
 import express from "express";
 import routes from "../routes";
-import { userDetail, editProfile, changePassword, users } from "../controllers/userController";
+import { userDetail, editProfile, changePassword } from "../controllers/userController";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, users);
 userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,10 +1,9 @@
 import express from "express";
 import routes from "../routes";
-import { videos, editVideo, deleteVideo, upload, videoDetail } from "../controllers/videoController";
+import { editVideo, deleteVideo, upload, videoDetail } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
