7cd495430bcb8c897c8498b3e73f31f6c753fc2b
kwanik-kor <rhksdlr134@naver.com>
2020-03-11 13:10:05 +0900

    get, post settings

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import routes from "../routes";
-import { editVideo, deleteVideo, upload, videoDetail } from "../controllers/videoController";
+import { editVideo, deleteVideo, getUpload, postUpload, videoDetail } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, postUpload);
+videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
