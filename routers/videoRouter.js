7cbaa13d581516d8bbe6a4693c97213ddafda221
kwanik-kor <rhksdlr134@naver.com>
2020-03-12 09:46:04 +0900

    mongoDB findOneAndRemove()

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,14 +1,22 @@
 import express from "express";
 import routes from "../routes";
-import { editVideo, deleteVideo, getUpload, postUpload, videoDetail } from "../controllers/videoController";
+import { deleteVideo, getUpload, postUpload, videoDetail, getEditVideo, postEditVideo } from "../controllers/videoController";
 import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
+//Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
+
+//Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+
+//Edit
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+//Delete
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
