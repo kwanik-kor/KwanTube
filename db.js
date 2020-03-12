cccb22b1d29a0c2a9e25885c242be5e07ae9d93f
kwanik-kor <rhksdlr134@naver.com>
2020-03-12 12:31:40 +0900

    webpack and polyfill scss to css, js

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 mongoose.connect(
-    process.env.MONGO_URL, 
+    process.env.MONGO_URL,
     { //configuration
-    useNewUrlParser: true, 
-    useFindAndModify: false
+        useNewUrlParser: true,
+        useFindAndModify: false
     }
 );
 
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+const ExtractCSS = require("extract-text-webpack-plugin");
+
+const MODE = process.env.WEBPACK_ENV;
+const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
+const OUTPUT_DIR = path.join(__dirname, "static");
+
+const config = {
+    entry: ["@babel/polyfill", ENTRY_FILE],
+    mode: MODE,
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                use: [
+                    {
+                        loader: "babel-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(scss)$/,
+                use: ExtractCSS.extract([
+                    {
+                        loader: "css-loader" //webpack can understand css
+                    },
+                    {
+                        loader: "postcss-loader", //css 호환성 해결
+                        options: {
+                            plugins() {
+                                return [autoprefixer({ browsers: "cover 99.5%" })];
+                            }
+                        }
+                    },
+                    {
+                        loader: "sass-loader" //scss or sass to css
+                    }
+                ])
+            }
+        ]
+    },
+    output: {
+        path: OUTPUT_DIR,
+        filename: "[name].js"
+    },
+    plugins: [new ExtractCSS("styles.css")]
+}
+
+module.exports = config;
